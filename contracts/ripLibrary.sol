73a48efe1b197016230ff5f7ac9628de73af4b1e
Massimo Omodei <massimo.omodei@logmein.com>
2022-02-10 11:37:47 +0100

    Publish the RIPToken exploit

---
+++ contracts/ripLibrary.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Ownable {
+    address private _owner;
+    
+    constructor() {
+        _owner = msg.sender;
+    }
+
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "You're not the Owner"); 
+        _;
+    }
+}
+
+contract Pausable {
+    bool public paused;
+
+    modifier notPaused() {
+        require(!paused, "Contract Paused");
+        _;
+    }
+}
